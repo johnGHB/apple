9d65faa821a602777862640cc74107e34ab8ab7b
John Nguyen <johnn.mail@icloud.com>
2017-03-20 08:31:07 -0500

    vpn initial commit

---
+++ vpn/vpn/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  vpn
+//
+//  Created by John Nguyen on 3/20/17.
+//  Copyright © 2017 John Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        NEVPNManager manager = [NEVPNManager sharedManager];
+        
+        int status = manager.connection.status;
+        
+        if (status == NEVPNStatusConnected) {
+            manager.connection stopVPNTunnel];
+        } else {
+            
+            [[NEVPNManager sharedManager] loadFromPreferencesWithCompletionHandler: ^(NSError *error) {
+                if (error) {
+                NSLog(@"Load error: %@", error);
+                }
+                else {
+                // No errors! The rest of your codes goes here...
+                NEVPNProtocolIPSec *p = [[NEVPNProtocolIPSec alloc] init];
+                p.serverAddress = @"VPN SERVER ADDRESS";
+                p.authenticationMethod = NEVPNIKEAuthenticationMethodCertificate;
+                p.localIdentifier = @"Local identifier";
+                p.remoteIdentifier = @"Remote identifier";
+                p.useExtendedAuthentication = YES;
+                p.identityData = [NSData dataWithBase64EncodedString:certBase64String];;
+                p.identityDataPassword = @"identity password";
+                p.disconnectOnSleep = NO;
+                
+                // Set protocol
+                [[NEVPNManager sharedManager] setProtocol:p];
+                
+                // Set on demand
+                NSMutableArray *rules = [[NSMutableArray alloc] init];
+                NEOnDemandRuleConnect *connectRule = [NEOnDemandRuleConnect new];
+                [rules addObject:connectRule];
+                [[NEVPNManager sharedManager] setOnDemandRules:rules];
+                
+                // Set localized description
+                [[NEVPNManager sharedManager] setLocalizedDescription:@"Description"];
+                
+                // Enable VPN
+                [[NEVPNManager sharedManager] setEnabled:YES];
+                
+                // Save to preference
+                [[NEVPNManager sharedManager] saveToPreferencesWithCompletionHandler: ^(NSError *error) {
+                NSLog(@"Save VPN to preference complete");
+                if (error) {
+                NSLog(@"Save to preference error: %@", error);
+                }
+                }];
+                }
+                }];
+        }
+        
+        func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
