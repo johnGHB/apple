ee42255bfd39d6467ab1a0b5ce9d58e0f96b14d3
John Nguyen <johnn.mail@icloud.com>
2017-04-09 20:15:38 -0500

    update

---
+++ ipsecvpn/ipsecvpn/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ipsecvpn
+//
+//  Created by John Nguyen on 4/9/17.
+//  Copyright © 2017 Emergnz. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ipsecvpn/ipsecvpn/vpnmanager.swift
@@ -0,0 +1,70 @@
+//
+//  vpnmanager.swift
+//  ipsecvpn
+//
+//  Created by John Nguyen on 4/9/17.
+//  Copyright © 2017 Emergnz. All rights reserved.
+//
+
+import Foundation
+import NetworkExtension
+import CoreData
+import Security
+
+
+class VPN {
+    
+    let vpnManager = NEVPNManager.shared();
+    
+    private var vpnLoadHandler: (Error?) -> Void { return
+    { (error:Error?) in
+        if ((error) != nil) {
+            print("Could not load VPN Configurations")
+            return;
+        }
+        let p = NEVPNProtocolIPSec()
+        p.username = "SOME_USERNAME"
+        p.serverAddress = "example.com"
+        p.authenticationMethod = NEVPNIKEAuthenticationMethod.sharedSecret
+        
+        let kcs = KeychainService();
+        kcs.save(key: "SHARED", value: "MY_SHARED_KEY")
+        kcs.save(key: "VPN_PASSWORD", value: "MY_PASSWORD")
+        p.sharedSecretReference = kcs.load(key: "SHARED")
+        p.passwordReference = kcs.load(key: "VPN_PASSWORD)
+            p.useExtendedAuthentication = true
+            p.disconnectOnSleep = false
+            self.vpnManager.protocolConfiguration = p
+            self.vpnManager.localizedDescription = "Contensi"
+            self.vpnManager.isEnabled = true
+            self.vpnManager.saveToPreferences(completionHandler: self.vpnSaveHandler)
+        } }
+    
+    private var vpnSaveHandler: (Error?) -> Void { return
+    { (error:Error?) in
+        if (error != nil) {
+            print("Could not save VPN Configurations")
+            return
+        } else {
+            do {
+                try self.vpnManager.connection.startVPNTunnel()
+            } catch let error {
+                print("Error starting VPN Connection \(error.localizedDescription)");
+            }
+        }
+        }
+        self.vpnlock = false
+    }}
+
+public func connectVPN() {
+    //For no known reason the process of saving/loading the VPN configurations fails.On the 2nd time it works
+    do {
+        try self.vpnManager.loadFromPreferences(completionHandler: self.vpnLoadHandler)
+    } catch let error {
+        print("Could not start VPN Connection: \(error.localizedDescription)" )
+    }
+}
+
+public func disconnectVPN() ->Void {
+    vpnManager.connection.stopVPNTunnel()
+}
